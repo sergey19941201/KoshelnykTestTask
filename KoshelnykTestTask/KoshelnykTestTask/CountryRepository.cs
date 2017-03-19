23c4b6c65b12a19d8b7c8182ccdb6434e9cd315d
sergey19941201 <koshelnick1994@mail.ru>
2017-03-19 03:32:49 +0200

    Moved to Repository

---
+++ KoshelnykTestTask/KoshelnykTestTask/CountryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoshelnykTestTask
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Response
+    {
+        public int count { get; set; }
+        public List<Item> items { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Response response { get; set; }
+    }
+}
--- KoshelnykTestTask/KoshelnykTestTask/GettingCountry.cs
+++ KoshelnykTestTask/KoshelnykTestTask/GettingCountry.cs
@@ -46,21 +46,4 @@ namespace KoshelnykTestTask
             return deserialized;
         }
     }
-
-    public class Item
-    {
-        public int id { get; set; }
-        public string title { get; set; }
-    }
-
-    public class Response
-    {
-        public int count { get; set; }
-        public List<Item> items { get; set; }
-    }
-
-    public class RootObject
-    {
-        public Response response { get; set; }
-    }
 }
