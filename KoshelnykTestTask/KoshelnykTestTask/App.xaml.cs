1208299f115c003d4f4d3355846fa32f1dbc0d9c
sergey19941201 <koshelnick1994@mail.ru>
2017-03-21 18:30:32 +0200

    Moved method that starts async operation from App to MainPage

--- KoshelnykTestTask/KoshelnykTestTask/App.xaml.cs
+++ KoshelnykTestTask/KoshelnykTestTask/App.xaml.cs
@@ -10,20 +10,5 @@ namespace KoshelnykTestTask
             //setting the MainPage
             MainPage = new NavigationPage(new KoshelnykTestTask.MainPage());
 		}
-
-        protected override void OnStart ()
-		{
-		   getCountry();
-		}
-
-        /*In this async method gettingCountry.FetchAsync(url) is called for retrieving data
-        and when it gets all the data, FillingPage() starts*/
-        private async void getCountry()
-        {
-            var url = "https://api.vk.com/api.php?oauth=1&method=database.getCountries&v=5.5&need_all=1&count=236";
-            GettingCountry gettingCountry = new GettingCountry();
-            await gettingCountry.FetchAsync(url);
-            await MainPage.Navigation.PushAsync(new FillingPage());
-        }
 	}
 }
--- KoshelnykTestTask/KoshelnykTestTask/MainPage.xaml.cs
+++ KoshelnykTestTask/KoshelnykTestTask/MainPage.xaml.cs
@@ -32,6 +32,17 @@ namespace KoshelnykTestTask
                     actInd,
                 }
             };
+            getCountry();//start getting countries
+        }
+
+        /*In this async method gettingCountry.FetchAsync(url) is called for retrieving data
+        and when it gets all the data, FillingPage() starts*/
+        private async void getCountry()
+        {
+            var url = "https://api.vk.com/api.php?oauth=1&method=database.getCountries&v=5.5&need_all=1&count=236";
+            GettingCountry gettingCountry = new GettingCountry();
+            await gettingCountry.FetchAsync(url);
+            await Navigation.PushAsync(new FillingPage());
         }
     }
 }
